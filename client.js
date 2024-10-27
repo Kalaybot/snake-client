155c537947f137f463ebabd7e475837e59081bc2
Kyle Bautista <[kylealexanderbautista09@gmail.com]>
2024-10-27 15:02:34 -0400

    let players know as soon as they are able to connect and intials all player in the game

--- client.js
+++ client.js
@@ -10,9 +10,14 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server.");
+    conn.write("Name: KAB")
+  });
+
   conn.on("data", (data) => {
     console.log("Server says: ", data);
-  })
+  });
 
   return conn;
 };
