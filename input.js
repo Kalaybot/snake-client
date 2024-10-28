77464c9f66fc1516d559079377eef7daa5753e53
Kyle Bautista <[kylealexanderbautista09@gmail.com]>
2024-10-27 20:28:06 -0400

    added actual move commands

--- input.js
+++ input.js
@@ -1,7 +1,10 @@
+let connection;
 
 // setup interface to handle user input from stdin
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn; // assigns parameter as variable value
+
   const stdin = process.stdin;  // create variable to hold the stdin object so we don't have to type process.stdin multiple times
   stdin.setRawMode(true); // Raw Mode allows us to listen for individual keypresses instead of waiting for the user to press enter
   stdin.setEncoding("utf8"); // utf8 encoding is set so that we can read the text data that is input
@@ -15,6 +18,17 @@ const setupInput = function () {
 const handleUserInput = function (key) {
   if (key === "\u0003") {
     process.exit();
+  }
+
+  // movement commands
+  if (key === "w") {
+    connection.write("Move: up");
+  } else if (key === "a") {
+    connection.write("Move: left");
+  } else if (key === "s") {
+    connection.write("Move: down");
+  } else if (key === "d") {
+    connection.write("Move: right");
   }
 };
 
